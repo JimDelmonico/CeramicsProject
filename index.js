3db06b105f07cddcab6a21e0d50321d6ba6dee08
domdomdom13 <daroneseno@gmail.com>
2015-12-12 08:23:42 +0000

    test

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', index);
 // app.use('/api', api);
-// app.use('/', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
